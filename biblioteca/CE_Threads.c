af25185e973c3b59b862eaf04ac02d64ef1d1d81
RAYO <rayodev16@gmail.com>
2025-07-03 18:15:14 -0600

    fea:create the function CE_Thread_create

---
+++ biblioteca/CE_Threads.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "CE_Threads.h"
+int contador=0;
+
+void CE_Thread_start_wrapper(CE_Thread *thread) {
+    thread->func(thread->arg);
+    thread->state = TERMINATED;
+    return ;
+    // Maybe yield to scheduler here
+}
+
+int CE_Thread_create(CE_Thread *thread,void (*func)(void *), void *arg){
+    contador++;
+    thread->id=contador;
+    thread->state=READY;
+    thread->func=func;
+    thread->arg=arg;
+    thread->stack=malloc(STACK_SIZE);
+    if(!thread->stack) return -1;
+    if (getcontext(&thread->context) == -1) return -1;
+    thread->context.uc_stack.ss_sp = thread->stack;
+    thread->context.uc_stack.ss_size = STACK_SIZE;
+    thread->context.uc_stack.ss_flags = 0;
+    thread->context.uc_link = NULL; 
+    makecontext(&thread->context,(void(*)())CE_Thread_start_wrapper,1,thread);
+    
+}
+
+
+
+
+
+
--- biblioteca/CE_Threads.h
+++ biblioteca/CE_Threads.h
@@ -1,6 +1,7 @@
 #ifndef  CE_Thread_H
 #define  CE_Thread_H
 #include <setjmp.h>  // to store execution context
+#include <ucontext.h>
 #define STACK_SIZE 8192
 
 
@@ -13,12 +14,15 @@ typedef enum {
 
 typedef struct  {
    int id;
-   jmp_buf context;
+   ucontext_t context;
    State state;    
    void *stack;
    void (*func)(void *); 
    void *arg;
 
 } CE_Thread;
+
+int CE_Thread_create(CE_Thread *thread,void (*func)(void *), void *arg);
+void CE_Thread_start_wrapper(CE_Thread *thread);
 
 #endif
